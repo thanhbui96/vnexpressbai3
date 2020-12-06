62c71ccda39a8761a6b4eca8ad2179de12448550
ThanhBui <buihuythanh33@gmail.com>
2020-12-06 11:04:24 +0700

    het bai 20

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,10 @@
+<?php
+ob_start();
+session_start();
+if(!isset($_SESSION["idUser"])&& $_SESSION["idGroup"]==1){
+    header("location:../index.php");
+}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -10,6 +17,11 @@
 <table width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
   <tr>
     <td id="hangTieuDe">TRANG QUẢN TRỊ</td>
+    <div style="width:200px; float:right">
+         <div>chào anh <?php echo $_SESSION["HoTen"] ?></div>
+    </div>
+
+    </td>
   </tr>
   <tr>
     <td id="hang2"><?php require "menu.php"; ?></td>
